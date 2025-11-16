fa2d204ca16fba6b1c6e355210ac50fd14ebc69e
ritzsaha <ritika.saha@mail.utoronto.ca>
2025-11-15 23:06:19 -0500

    Loading animation before weather is shown

--- src/main/java/com/aurora/climatesync/view/Main.java
+++ src/main/java/com/aurora/climatesync/view/Main.java
@@ -7,9 +7,28 @@ import com.aurora.climatesync.service.WeatherService;
 import java.util.List;
 
 public class Main {
+
+    // Loading
+    public static void showLoading(String message){
+        System.out.println(message);
+
+        try{
+            for (int i = 0; i < 3; i++){
+              Thread.sleep(500);
+              System.out.println(".");
+            }
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("\n");
+    }
+
     public static void main(String[] args) {
         WeatherService service = new WeatherService();
         Location toronto = new Location("Toronto", "Canada");
+
+        showLoading("Fetching Weather Data " + toronto.getCityName() + "...");
 
         List<WeatherForecast> forecasts = service.getWeeklyForecast(toronto);
         System.out.println("=== 7-Day Weather Forecast for " + toronto + "===\n");
