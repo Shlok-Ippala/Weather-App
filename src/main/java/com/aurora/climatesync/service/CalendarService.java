621d9d12ded4f4dc6e9083206dfc669511d34b9f
William Bo <william.zhi.bo@gmail.com>
2025-11-25 01:07:21 -0500

    Refactor Calendar Service: Add EventMapper and update interface for optimization

--- src/main/java/com/aurora/climatesync/service/CalendarService.java
+++ src/main/java/com/aurora/climatesync/service/CalendarService.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public interface CalendarService {
     List<CalendarEvent> getUpcomingEvents();
-    void addEvent(CalendarEvent event);
+    List<CalendarEvent> getUpcomingEvents(int maxResults);
+    CalendarEvent addEvent(CalendarEvent event);
     void updateEvent(CalendarEvent event);
     void deleteEvent(String eventId);
     String connect() throws Exception;
